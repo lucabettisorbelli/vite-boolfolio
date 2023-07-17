68c695ae3789481088a74ede21e109a276a260ad
luca betti sorbelli <luca betti sorbelli>
2023-07-17 21:50:25 +0200

    fix appMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,22 +5,42 @@ export default {
     name: "AppMain",
     data() {
         return {
-            chiave: "valore"
+            chiave: "valore",
+            apiUrl: "http://127.0.0.1:8000/api/projects",
+            loading: false,
+            loadingError: false,
+            projects: []
         }
     },
     methods: {
+        getProjects() {
+            this.loading = true;
+            axios.get(this.apiUrl).then(response => {
+                console.log(response.data.results);
+                this.projects = response.data.results;
+                this.loading = false;
+            }).catch(err => {
+                this.loading = false;
+                this.loadingError = err.message;
+            })
 
+        }
     },
     mounted() {
-        axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
-            console.log(response);
-        })
+        this.getProjects();
     }
 }
+
 </script>
 
 <template>
     <main>
         <div>Componente</div>
+        <h2 v-if="loading">Caricamento in corso</h2>
+        <h2 v-if="loadingError"> {{ loadingError }} </h2>
+
+        <div v-for="project in projects">
+            <h2>{{ project }}</h2>
+        </div>
     </main>
 </template>
