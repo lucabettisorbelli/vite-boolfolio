111a7441f873bf3a6c8afa1fc95b541589fa2fe9
luca betti sorbelli <luca betti sorbelli>
2023-07-17 17:46:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: "AppVue",
+
+  data() {
+    return {}
+  },
+  components: {
+    AppMain,
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Api Laravel + Vue</h1>
+    <AppMain />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from "axios";
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            chiave: "valore"
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+            console.log(response);
+        })
+    }
+}
+</script>
+
+<template>
+    <main>
+        <div>Componente</div>
+    </main>
+</template>
